2c682d90584f5cabbadb4841bfe1ee4db4c3f012
Fatimah Nadia Eka Putri <114746979+fatimahnadiaekaputri@users.noreply.github.com>
2023-12-11 13:36:16 +0700

    Update CatatTumbuhAnak.xaml.cs

--- CatatTumbuhAnak.xaml.cs
+++ CatatTumbuhAnak.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Data.SqlClient;
 using System.Data;
+using System.Globalization;
 
 namespace siKecil
 {
@@ -21,15 +22,23 @@ namespace siKecil
     /// </summary>
     public partial class CatatTumbuhAnak : Window
     {
+
         private readonly string User_ID;
         Connection connectionHelper = new Connection();
         public CatatTumbuhAnak(string User_ID)
-        { 
+        {
             InitializeComponent();
             this.User_ID = User_ID;
+            Loaded += CatatTumbuhAnak_Loaded;
         }
 
-         private void SaveCatatanTumbuhAnak(object sender, RoutedEventArgs e)
+        private void CatatTumbuhAnak_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadDataGrid(User_ID);
+        }
+
+
+        private void SaveCatatanTumbuhAnak(object sender, RoutedEventArgs e)
         {
             using (SqlConnection sqlCon = connectionHelper.GetConn())
             {
@@ -46,24 +55,22 @@ namespace siKecil
                         using (SqlCommand checkCmd2 = new SqlCommand(checkQuery, sqlCon, transaction))
                         {
                             checkCmd2.Parameters.AddWithValue("@User_ID", User_ID);
-                            String query = "INSERT INTO CatatTumbuhAnak (User_ID, Tanggal, Berat, Tinggi, LingkarKepala) VALUES (@User_ID, @Tanggal, @Berat, @Tinggi, @LingkarKepala)";
+                            String query = "INSERT INTO CatatTumbuhAnak (User_ID, No, Tanggal, Berat, Tinggi, LingkarKepala) VALUES (@User_ID, (SELECT ISNULL(MAX(No), 0) + 1 FROM CatatTumbuhAnak WHERE User_ID = @User_ID), @Tanggal, @Berat, @Tinggi, @LingkarKepala)";
                             using (SqlCommand cmd = new SqlCommand(query, sqlCon, transaction))
                             {
                                 cmd.Parameters.AddWithValue("@User_ID", User_ID);
                                 cmd.Parameters.AddWithValue("@Tanggal", datePicker.DisplayDate);
                                 cmd.Parameters.AddWithValue("@Berat", txtBerat.Text);
                                 cmd.Parameters.AddWithValue("@Tinggi", txtTinggi.Text);
-                                cmd.Parameters.AddWithValue("@LingkarKepala", txtLingkarKepala.Text);                                
+                                cmd.Parameters.AddWithValue("@LingkarKepala", txtLingkarKepala.Text);
                                 cmd.ExecuteNonQuery();
                             }
                         }
 
                         transaction.Commit();
                         MessageBox.Show("Data berhasil disimpan");
+                        LoadDataGrid(User_ID);
                     }
-
-                    
-
                     catch (Exception ex)
                     {
                         transaction.Rollback();
@@ -72,11 +79,7 @@ namespace siKecil
                 }
             }
         }
-
-        private void tampilDataGrid(object sender, RoutedEventArgs e)
-        {
-            LoadDataGrid(User_ID);
-        }
+        
         private void LoadDataGrid(string User_ID)
         {
             try
@@ -85,7 +88,7 @@ namespace siKecil
                 {
                     sqlCon.Open();
 
-                    string showQuery = $"SELECT Tanggal, Berat, Tinggi, LingkarKepala FROM CatatTumbuhAnak WHERE User_Id = {User_ID}";
+                    string showQuery = $"SELECT No, Tanggal, Berat, Tinggi, LingkarKepala FROM CatatTumbuhAnak WHERE User_Id = {User_ID}";
                     using (SqlCommand cmd = new SqlCommand(showQuery, sqlCon))
                     {
                         using (SqlDataReader reader = cmd.ExecuteReader())
@@ -94,24 +97,22 @@ namespace siKecil
                             {
                                 DataSet dataSet = new DataSet();
                                 dataSet.Load(reader, LoadOption.OverwriteChanges, "CatatTumbuhAnak");
+
                                 dataGrid.AutoGeneratingColumn += (sender, e) =>
                                 {
-                                    if (e.PropertyName == "Tanggal")
+                                    if (e.PropertyName == "Tanggal" && e.Column is DataGridTextColumn textColumn)
                                     {
-                                        var textColumn = e.Column as DataGridTextColumn;
-                                        if (textColumn != null)
-                                        {
-                                            textColumn.Binding.StringFormat = "dd/MM/yyyy";
-                                        }
+                                        textColumn.Binding.StringFormat = "d MMMM yyyy";
                                     }
                                 };
+
                                 dataGrid.ItemsSource = dataSet.Tables["CatatTumbuhAnak"].DefaultView;
                                 dataGrid.IsReadOnly = true;
                             }
                         }
                     }
                 }
-            }    
+            }
             catch (Exception ex)
             {
                 MessageBox.Show("Error: " + ex.Message);
@@ -149,5 +150,63 @@ namespace siKecil
                 txtLingkarKepala.Text = "";
             }
         }
+
+        private void updateData(object sender, RoutedEventArgs e)
+        {
+            if (dataGrid.SelectedItem != null)
+            {
+                DataRowView selectedRow = (DataRowView)dataGrid.SelectedItem;
+
+                using (SqlConnection sqlCon = connectionHelper.GetConn())
+                {
+                    sqlCon.Open();
+
+                    string selectedNo = selectedRow["No"].ToString();
+
+                    string updateQuery = "UPDATE CatatTumbuhAnak SET Tanggal = @Tanggal, Berat = @Berat, Tinggi = @Tinggi, LingkarKepala = @LingkarKepala WHERE No = @No";
+                    using (SqlCommand updateCommand = new SqlCommand(updateQuery, sqlCon))
+                    {
+                        
+                        updateCommand.Parameters.AddWithValue("@Tanggal", datePicker.DisplayDate);
+                        updateCommand.Parameters.AddWithValue("@Berat", txtBerat.Text);
+                        updateCommand.Parameters.AddWithValue("@Tinggi", txtTinggi.Text);
+                        updateCommand.Parameters.AddWithValue("@LingkarKepala", txtLingkarKepala.Text);
+                        updateCommand.Parameters.AddWithValue("@User_ID", User_ID);
+                        updateCommand.Parameters.AddWithValue("@No", selectedNo);
+
+                        updateCommand.ExecuteNonQuery();
+                        MessageBox.Show("Data berhasil di update!");
+
+                        LoadDataGrid(User_ID);
+                    }
+                }
+            }
+        }
+        private void deleteData(object sender, RoutedEventArgs e)
+        {
+            if (dataGrid.SelectedItem != null)
+            {
+                DataRowView selectedRow = (DataRowView)dataGrid.SelectedItem;
+
+                using (SqlConnection sqlCon = connectionHelper.GetConn())
+                {
+                    sqlCon.Open();
+
+                    string selectedNo = selectedRow["No"].ToString();
+
+                    string deleteQuery = "DELETE FROM CatatTumbuhAnak WHERE No = @No";
+                    using (SqlCommand deleteCommand = new SqlCommand(deleteQuery, sqlCon))
+                    {
+                        deleteCommand.Parameters.AddWithValue("@No", int.Parse(selectedNo));
+                        deleteCommand.ExecuteNonQuery();
+                        MessageBox.Show("Data berhasil dihapus!");
+                    }
+                    LoadDataGrid(User_ID); 
+                }
+            }
+
+        }
     }
 }
+
+
